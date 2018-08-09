e28f898d8f7908f26bdca04a70ce1095dff6a77e
joseph <jpatrick148@gmail.com>
2018-08-09 00:12:48 -0400

    Exception Mostly Working
    Signed-off-by: joseph <jpatrick148@gmail.com>

---
+++ Lab1update.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1, num2;
+
+            try
+            {
+                
+            while (true)
+            {
+                Console.WriteLine("Please Enter 2 Numbers ");
+                num1 = Console.ReadLine();
+                num2 = Console.ReadLine();
+
+
+                if (num1.Length == num2.Length)
+                {
+                    if (TotalNumericString(num1) == TotalNumericString(num2))
+                    {
+                        Console.WriteLine("True");
+                    }
+                    else
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+                else
+
+                {
+                    Console.WriteLine("The numbers must be the same length. Try again.");
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Do you want to run this program again [Y/N] ?");
+                    string answer = Console.ReadLine().ToUpper();
+                    if (answer == "Y")
+                        break;
+                    if (answer == "N")
+                        return;
+                }
+            }            
+                int TotalNumericString(string str)
+                {
+                    int total = 0;
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        char c = str[i];
+                        total += Convert.ToInt32(c.ToString());
+
+                    }
+                    return total;
+                }
+            }
+            catch (Exception ex)
+            {
+                while (true)
+                {
+                    Console.WriteLine( "Please Use Whole Numbers \nDo you want to run this program again [Y/N] ?");
+                    string answer = Console.ReadLine().ToUpper();
+                    if (answer == "Y")
+                        break;
+                    if (answer == "N")
+                        return;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num1, num2;
+            int total1 = 0;
+
+            Console.WriteLine("Please Enter 2 Numbers ");
+            num1 = Console.ReadLine();
+            num2 = Console.ReadLine();
+
+
+            if (num1.Length == num2.Length)
+
+                for (int i = 0; i < num1.Length; i++)
+                {
+                    char c = num1[i];
+                    total1 += Convert.ToInt32((string)c);
+                }
+
+            else
+
+            {
+                Console.WriteLine("The numbers must be the same length. Try again.");
+            }
+            Console.WriteLine(total1);
+            Console.ReadLine();
+
+        }
+    }
+        public int totalNumericString(string s)
+        {
+            int total = 0;
+            for (int i = 0; i < num1.Length; i++)
+            {
+                char c = num1[i];
+                total += Convert.ToInt32((string)c);
+            }
+            return total;
+        }
+
+}
